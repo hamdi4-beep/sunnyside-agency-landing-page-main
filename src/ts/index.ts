850c86a6d03d8b788976e3897eb0f46f8801f47e
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-05-22 10:23:01 -0700

    commited new changes

--- src/ts/index.ts
+++
@@ -1,4 +0,0 @@
-const navIcon = document.querySelector('.nav-icon') as HTMLDivElement
-const navItems = document.querySelector('.nav-items') as HTMLUListElement
-
-navIcon.addEventListener('pointerdown', (e: Event) => navItems.classList.toggle('show'))
