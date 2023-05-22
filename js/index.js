43359d4c37dfed0bb14bd233b384ed1976dcaa2d
Hamdi Kamel <hamdi.pirillo4@gmail.com>
2023-05-22 11:00:43 -0700

    finished making the mobile nav bar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 "use strict";
 const navIcon = document.querySelector('.nav-icon');
-const navItems = document.querySelector('.nav-items');
-navIcon.addEventListener('pointerdown', (e) => navItems.classList.toggle('show'));
+navIcon.addEventListener('pointerdown', e => {
+    const navItems = document.querySelector('.nav-items');
+    navItems.classList.toggle('hidden');
+});
---
+++ src/ts/index.ts
@@ -0,0 +1,6 @@
+const navIcon = document.querySelector('.nav-icon') as HTMLDivElement
+
+navIcon.addEventListener('pointerdown', e => {
+    const navItems = document.querySelector('.nav-items') as HTMLDivElement
+    navItems.classList.toggle('hidden')
+})
